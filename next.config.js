5409daf6e6fd1e6ce5815a4d5871439c28b78b6d
Bramandia Gilanglaksana Adam <gilangbram@gmail.com>
2022-03-05 07:47:50 +0700

    :zap: setup.env in next.config for frontend

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+const webpack = require('webpack');
+const { parsed: myEnv } = require('dotenv').config({
+  path: '.env',
+});
+
+
+module.exports = {
+  nextConfig,
+  webpack(config) {
+    config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
+    return config;
+  },
+};
